fb3146deff77ce9e2c809548dd9a5e0bd06c5aef
Steelver <1steelver@gmail.com>
2022-06-24 23:54:29 +0000

    .....

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [isGreen, setIsGreen] = useState(false);
+	const [isYellow, setIsYellow] = useState(false);
+	const [isRed, setIsRed] = useState(false);
+	const encenderVerde = () => {
+		setIsVerde(true);
+		setIsAmarillo(false);
+		setIsRojo(false);
+	};
+
+	const encenderAmarillo = () => {
+		setIsVerde(false);
+		setIsAmarillo(true);
+		setIsRojo(false);
+	};
+	const encenderRojo = () => {
+		setIsVerde(false);
+		setIsAmarillo(false);
+		setIsRojo(true);
+	};
+	const rectangulo = {
+		width: "180px",
+		height: "380px",
+		border: "4px solid blak",
+		background: "#000000",
+	};
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div
+				className="d-flex flex-column mt-5 text-center mx-auto"
+				style={rectangulo}>
+				<div className="col">
+					<button
+						onClick={() => encenderVerde()}
+						className={`${
+							isVerde === true ? "bg-success" : ""
+						} rounded-circle p-5 `}></button>
+				</div>
+				<div className="col">
+					<button
+						onClick={() => encenderAmarillo()}
+						className={`${
+							isAmarillo === true ? "bg-warning" : ""
+						} rounded-circle p-5`}></button>
+				</div>
+				<div className="col">
+					<button
+						onClick={() => encenderRojo()}
+						className={`${
+							isRojo === true ? "bg-danger" : ""
+						} rounded-circle p-5`}></button>
+				</div>
+			</div>
 		</div>
 	);
 };
